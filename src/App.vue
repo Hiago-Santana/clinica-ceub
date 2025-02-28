f9f3e90ee48e7dd94b790585b752eae8e305d6c9
Hiago-Santana <hiago_douglas@hotmail.com>
2025-02-28 15:34:39 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <router-view>
-    
-  </router-view>
+  <div class="flex">
+    <div class="flex-auto">
+      <nav-bar></nav-bar>
+      <router-view>
+      </router-view>
+    </div>
+  </div>
+
+
 </template>
 <script setup>
+import NavBar from './layouts/NavBar.vue';
 </script>
---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="relative z-50 bg-gray-800 dark:bg-transparent h-12">
+
+        <div class="block md:hidden ">
+            <div class="flex w-full h-full justify-between items-center">
+                <span class=" h-12 flex mx-4 justify-center items-center">
+                    <button @click='showBar = true' class="flex">
+                        <span
+                            class="material-symbols-outlined text-white hover:text-second dark:text-second">menu</span>
+                    </button>
+                </span>
+                <div class="">
+                    <img src="/ceub-logo.png" alt="AgendaCheck" class="pl-4">
+                </div>
+                <a href="#" @click="goTo('login')"
+                    class="flex border-2 border-white rounded-xl py-1 px-3 text-white dark:text-white md:dark:text-blue-500 mx-4">
+                    Entrar
+                    <span class="ml-2 material-symbols-outlined text-white">
+                        login
+                    </span>
+                </a>
+            </div>
+        </div>
+
+        <div class="w-full h-full flex justify-between mx-auto items-center">
+            <div class="flex justify-start">
+                <img src="/ceub-logo.png" alt="AgendaCheck" class="pl-4 hidden md:block mr-2">
+                <div class="self-center text-2xl font-russo text-white hidden md:block">AgendaCheck</div>
+            </div>
+            <div class="">
+                <ul class="w-full flex justify-end items-center ">
+                    <li>
+                        <a href="#"
+                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-blue-500">Download</a>
+                    </li>
+                    <li>
+                        <a href="#" @click="goTo('login')"
+                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-blue-500"
+                            aria-current="page">Criar Conta</a>
+                    </li>
+                    <li>
+                        <a href="#" @click="goTo('login')"
+                            class=" border-2 border-white rounded-xl hidden md:block py-1 px-3 text-white dark:text-white md:dark:text-blue-500 mx-4">
+                            <div class="flex">
+                                Entrar
+                                <span class="ml-2 material-symbols-outlined text-white">
+                                    login
+                                </span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <side-bar-welcome @close-bar="closeBar()" :showBar="showBar"></side-bar-welcome>
+    </nav>
+</template>
+<script setup>
+</script>
