3598546b5e92363f94faf883ff7bac5de737c063
Hiago Santana <hiago_douglas@hotmail.com>
2025-03-02 22:31:49 -0300

    Update

--- src/layouts/NavBar.vue
+++ src/layouts/NavBar.vue
@@ -1,16 +1,16 @@
 <template>
-    <nav class="relative z-50 bg-gray-800 dark:bg-transparent h-12">
+    <nav class="relative z-50 bg-second dark:bg-second h-12">
 
-        <div class="block md:hidden ">
+        <div class="block md:hidden">
             <div class="flex w-full h-full justify-between items-center">
                 <span class=" h-12 flex mx-4 justify-center items-center ">
                     <button @click='showBar = true' class="flex">
                         <span
-                            class="material-symbols-outlined text-white hover:text-second dark:text-second">menu</span>
+                            class="material-symbols-outlined text-white hover:text-first dark:text-white">menu</span>
                     </button>
                 </span>
                 <div class="">
-                    <img src="/ceub-logo.png" alt="Clinica Ceub" class="pl-2 h-12">
+                    <img src="/logo-ceub-white.webp" alt="Clinica Ceub" class="pl-2 h-12">
                 </div>
                 <a href="#" @click="goTo('login')"
                     class="flex border-2 border-white rounded-xl py-1 px-3 text-white dark:text-white md:dark:text-blue-500 mx-4">
@@ -24,23 +24,23 @@
 
         <div class="w-full h-full flex justify-between mx-auto items-center">
             <div class="flex justify-start">
-                <img src="/ceub-logo.png" alt="Clinica Ceub" class="pl-2 hidden md:block mr-2 h-12">
+                <img src="/logo-ceub-white.webp" alt="Clinica Ceub" class="pl-2 hidden md:block mr-2 h-12">
                 <!-- <div class="self-center text-2xl font-russo text-white hidden md:block">AgendaCheck</div> -->
             </div>
             <div class="">
                 <ul class="w-full flex justify-end items-center ">
                     <li>
                         <a href="#"
-                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-blue-500">Download</a>
+                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-white">Download</a>
                     </li>
                     <li>
                         <a href="#" @click="goTo('login')"
-                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-blue-500"
+                            class="hidden md:block py-2 px-3 text-white dark:text-white md:dark:text-white"
                             aria-current="page">Criar Conta</a>
                     </li>
                     <li>
                         <a href="#" @click="goTo('login')"
-                            class=" border-2 border-white rounded-xl hidden md:block py-1 px-3 text-white dark:text-white md:dark:text-blue-500 mx-4">
+                            class=" border-2 border-white rounded-xl hidden md:block py-1 px-3 text-white dark:text-white md:dark:text-white mx-4">
                             <div class="flex">
                                 Entrar
                                 <span class="ml-2 material-symbols-outlined text-white">
--- src/pages/landingPage/HeroSection.vue
+++ src/pages/landingPage/HeroSection.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class="w-full bg-black">
-        <h1 class="">Hero Page</h1>
+    <div class="w-full bg-first h-screen justify-center items-center flex">
+        <div class="max-w-7xl">
+            <div class="w-screen self-center items-center ">
+                <h1>
+                    <span class="text-second font-lexend">Exames </span>
+                    <span>e</span>
+                </h1>
+                <h1 class="text-second font-lexend">Consultas</h1>
+                <h1>em um só</h1>
+                <h1>Lugar</h1>
+            </div>
+        </div>
+
     </div>
 
 </template>
