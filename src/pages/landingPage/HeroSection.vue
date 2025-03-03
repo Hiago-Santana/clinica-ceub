f85a94f1b1feb5fa68c14b2d249ade89800be49c
Hiago-Santana <hiago_douglas@hotmail.com>
2025-03-03 17:37:25 -0300

    update

--- src/pages/landingPage/HeroSection.vue
+++ src/pages/landingPage/HeroSection.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="w-full bg-first h-screen justify-center items-center flex">
         <div class="max-w-7xl">
-            <div class="w-screen self-center items-center ">
-                <h1>
-                    <span class="text-second font-lexend">Exames </span>
-                    <span>e</span>
-                </h1>
-                <h1 class="text-second font-lexend">Consultas</h1>
-                <h1>em um só</h1>
-                <h1>Lugar</h1>
+            <div class="grid md:grid-cols-2 justify-center items-center">
+                <div class=" bg-black">
+                    <h1 class="">
+                        <span class="text-second font-lexend font-bold">Exames </span>
+                        <span>e</span>
+                    </h1>
+                    <h1 class="text-second font-lexend font-bold">Consultas</h1>
+                    <h1>em um só</h1>
+                    <h1>Lugar</h1>
+                </div>
+                <div class="w-full ">
+                    <img src="/public/doctor.png" alt="" class="h-120">
+                </div>
             </div>
+
         </div>
+
 
     </div>
 
