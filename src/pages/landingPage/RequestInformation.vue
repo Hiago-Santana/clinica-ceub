7ecc487d7ce8f553c89df78a59a72c0aeac2755c
Hiago-Santana <hiago_douglas@hotmail.com>
2025-03-05 16:19:54 -0300

    Update

--- src/pages/landingPage/RequestInformation.vue
+++ src/pages/landingPage/RequestInformation.vue
@@ -8,19 +8,19 @@
                 class="rounded-lg justify-center px-4 pt-4 border-1 border-gray-200 shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
                 <div class="grid grid-auto-rows w-full gap-4">
                     <input type="text" placeholder="Nome"
-                        class="w-full  border border-gray-200 rounded-md focus:border-gray-200 focus:ring-0 shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
+                        class="w-full capitalize px-2 border text-gray-800 border-gray-200 rounded-md focus:outline-none shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
                     <input type="text" placeholder="Email"
-                        class="w-full border border-gray-200 rounded-md shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
+                        class="w-full px-2 border text-gray-800 border-gray-200 rounded-md focus:outline-none shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
                     <div class="grid grid-cols-2 gap-4">
                         <input type="text" placeholder="Cidade"
-                        class=" w-full border border-gray-200 rounded-md shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
+                        class=" w-full px-2 capitalize border text-gray-800 border-gray-200 rounded-md focus:outline-none shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
                         <input type="text" placeholder="Estado"
-                        class="w-full border border-gray-200 rounded-md shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
+                        class="w-full px-2 capitalize border text-gray-800 border-gray-200 rounded-md focus:outline-none shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">
                     </div>
-                    <textarea type="text" rows="5" placeholder="Solicitação" class="w-full border-1 border-gray-200 rounded-md shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]"></textarea>
+                    <textarea type="text" rows="5" placeholder="Solicitação" class="w-full px-2 text-gray-800 border-1 border-gray-200 rounded-md focus:outline-none shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]"></textarea>
                 </div>
                 <div class="flex w-full justify-center items-center">
-                    <button type="submit" class="rounded-2xl border border-gray-200 m-6 text-first w-25 shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">Enviar</button>
+                    <button type="submit" class="rounded-2xl border border-gray-200 m-6 text-first w-25 hover:bg-first hover:border-first hover:text-white shadow-[0_4px_4px_2px_rgba(0,0,0,0.3)]">Enviar</button>
                 </div>                
             </form>
         </div>
