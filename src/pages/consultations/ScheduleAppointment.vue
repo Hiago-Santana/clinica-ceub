85e097553ac5910b418050ac9a8d1842d7ecac3d
Hiago-Santana <hiago_douglas@hotmail.com>
2025-03-24 16:39:09 -0300

    Update

--- src/pages/consultations/ScheduleAppointment.vue
+++ src/pages/consultations/ScheduleAppointment.vue
@@ -1,14 +1,13 @@
 <template>
-<div class="h-screen w-screen">
-    <vue-cal
-        ref="exCreateEventsVueCalEl"
-        editable-events
-        @cell-dblclick="createEvent">
-    ></vue-cal>
-</div>
+    <div class="h-screen w-screen">
+        <vue-cal 
+            ref="exCreateEventsVueCalEl" editable-events @cell-dblclick="createEvent"
+            :time-from="6 * 60" :time-to="17 * 60" :time-step="30">
+            ></vue-cal>
+    </div>
 </template>
 <script setup>
-import { ref, onMounted  } from 'vue'
+import { ref, onMounted } from 'vue'
 import { VueCal } from 'vue-cal'
 import 'vue-cal/style'
 
@@ -16,11 +15,18 @@ onMounted(() => {
     console.log("VueCal carregado:", exCreateEventsVueCalEl.value);
 });
 
-const exCreateEventsVueCalEl = ref(null)
-const createEvent = ( event, cursor ) => {
-    console.log("Duplo clique acionado!", cursor);
-    event.start = cursor.date;
-    event.end = cursor.date.addHours(1);
-    exCreateEventsVueCalEl.value.view.createEvent(event)
-}
+const exCreateEventsVueCalEl = ref(null);
+
+const createEvent = ({ cursor }) => {
+  if (!exCreateEventsVueCalEl.value || !exCreateEventsVueCalEl.value.view) return;
+
+  const newEvent = {
+    start: cursor.date,
+    end: new Date(cursor.date.getTime() + 60 * 60 * 1000), // 1 hora depois
+    title: 'Novo Evento',
+  };
+
+  exCreateEventsVueCalEl.value.view.createEvent(newEvent);
+};
+
 </script>
