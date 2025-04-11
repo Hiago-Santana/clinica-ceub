db514cefea007be1691c57a33a365e6792f6f9c4
Hiago-Santana <hiago_douglas@hotmail.com>
2025-04-11 17:38:57 -0300

    update

--- src/pages/consultations/Calendar.vue
+++ src/pages/consultations/Calendar.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="w-full px-2">
+        <div class="bg-red-500">
+             
+        </div>
+        
+   
         <div class="flex bg-blue-900 rounded-md">
+
             <button @click="delayWeek()" class="flex w-8 h-12 rounded-md self-center justify-center bg-second">
                 <span class="material-symbols-outlined self-center">
                     chevron_left
@@ -17,7 +23,7 @@
                         <div>Sex</div>
                         <div>Sab</div>
                     </div>
-                    <div  class="grid grid-cols-7 justify-items-center">
+                    <div class="grid grid-cols-7 justify-items-center">
                         <div v-for="daysOfTheWeeks in daysOfTheWeek" :key="daysOfTheWeeks">
                             {{ daysOfTheWeeks }}
                         </div>
@@ -34,42 +40,54 @@
 </template>
 <script setup>
 import { onMounted, ref } from 'vue';
-import { subDays, addDays } from 'date-fns';
+import { subDays, addDays, format  } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 const daysOfTheWeek = ref([]);
+const firstDayOfTheWeek = ref();
+const monthOfTheWeek = ref();
 
 onMounted(() => {
-    getDaysOfTheWeek(getfirstDayOfTheWeek());
+    //getDaysOfTheWeek(getfirstDayOfTheWeek());
+    startCalendar()
 })
 
 
-function getDaysOfTheWeek(firstDayOfTheWeek) {
-    
+function getDaysOfTheWeek() {
+
     daysOfTheWeek.value = [];
     for (let i = 0; i < 7; i++) {
-        daysOfTheWeek.value.push(addDays(firstDayOfTheWeek, i).getDate());
+        daysOfTheWeek.value.push(addDays(firstDayOfTheWeek.value, i).getDate());
     }
 }
 
-function getfirstDayOfTheWeek() {
-    const today = new Date();
-    const dayOfTheWeekToday = today.getDay();
-    const firstDayOfTheWeek = new Date(today);
-    firstDayOfTheWeek.setDate(today.getDate() - dayOfTheWeekToday)
-    console.log("firstDayOfTheWeek ", firstDayOfTheWeek)
-    return firstDayOfTheWeek;
+function getfirstDayOfTheWeek(date) {
+    const dayOfTheWeekToday = date.getDay();
+    firstDayOfTheWeek.value = new Date(date);
+    firstDayOfTheWeek.value.setDate(date.getDate() - dayOfTheWeekToday);
 }
 
 const advanceWeek = () => {
-    for(let i = 0; i < daysOfTheWeek.value.length; i++){
-        daysOfTheWeek.value[i] += 7
-    }
+    firstDayOfTheWeek.value = addDays(firstDayOfTheWeek.value, 7);
+    getDaysOfTheWeek();
+    getMonthOfTheWeek()
 }
 
 const delayWeek = () => {
-    for(let i = 0; i < daysOfTheWeek.value.length; i++){
-        daysOfTheWeek.value[i] -= 7
-    }
+    firstDayOfTheWeek.value = subDays(firstDayOfTheWeek.value, 7);
+    getDaysOfTheWeek();
+    getMonthOfTheWeek();
+}
+
+const startCalendar = () => {
+    const today = new Date();
+    getfirstDayOfTheWeek(today);
+    getDaysOfTheWeek();
+    getMonthOfTheWeek()
+}
+
+const getMonthOfTheWeek = () => {
+    monthOfTheWeek.value = format(firstDayOfTheWeek.value, "MMMM", { locale: ptBR });
 }
 
 
