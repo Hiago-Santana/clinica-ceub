9f636bc4d0f3a4fd9ce0577f8835a45a8e6aaf20
Hiago-Santana <hiago_douglas@hotmail.com>
2025-04-14 16:32:52 -0300

    update

--- src/pages/consultations/Calendar.vue
+++ src/pages/consultations/Calendar.vue
@@ -1,46 +1,51 @@
 <template>
     <div class="w-full px-2">
         <div class="bg-red-500">
-             
-        </div>
-        
-   
-        <div class="flex bg-blue-900 rounded-md">
 
-            <button @click="delayWeek()" class="flex w-8 h-12 rounded-md self-center justify-center bg-second">
-                <span class="material-symbols-outlined self-center">
-                    chevron_left
-                </span>
-            </button>
-            <div class="flex-auto">
-                <div class="grid grid-rows-2">
-                    <div class="grid grid-cols-7 justify-items-center font-bold">
-                        <div>Dom</div>
-                        <div>Seg</div>
-                        <div>Ter</div>
-                        <div>Qua</div>
-                        <div>Qui</div>
-                        <div>Sex</div>
-                        <div>Sab</div>
-                    </div>
-                    <div class="grid grid-cols-7 justify-items-center">
-                        <div v-for="daysOfTheWeeks in daysOfTheWeek" :key="daysOfTheWeeks">
-                            {{ daysOfTheWeeks }}
+        </div>
+
+        <div class="grid grid-rows-2 bg-blue-900 rounded-md">
+            <div class="grid justify-items-center self-center text-xl font-semibold">
+                {{ monthOfTheWeek }}
+            </div>
+            <div class="flex ">
+
+                <button @click="delayWeek()" class="flex w-8 h-12 rounded-md self-center justify-center bg-second">
+                    <span class="material-symbols-outlined self-center">
+                        chevron_left
+                    </span>
+                </button>
+                <div class="flex-auto">
+                    <div class="grid grid-rows-2">
+                        <div class="grid grid-cols-7 justify-items-center font-bold">
+                            <div>Dom</div>
+                            <div>Seg</div>
+                            <div>Ter</div>
+                            <div>Qua</div>
+                            <div>Qui</div>
+                            <div>Sex</div>
+                            <div>Sab</div>
+                        </div>
+                        <div class="grid grid-cols-7 justify-items-center">
+                            <div v-for="daysOfTheWeeks in daysOfTheWeek" :key="daysOfTheWeeks">
+                                {{ daysOfTheWeeks }}
+                            </div>
                         </div>
                     </div>
                 </div>
+                <button @click="advanceWeek()" class="flex w-8 h-12 rounded-md self-center justify-center bg-second">
+                    <span class="material-symbols-outlined self-center">
+                        chevron_right
+                    </span>
+                </button>
             </div>
-            <button @click="advanceWeek()" class="flex w-8 h-12 rounded-md self-center justify-center bg-second">
-                <span class="material-symbols-outlined self-center">
-                    chevron_right
-                </span>
-            </button>
         </div>
+
     </div>
 </template>
 <script setup>
 import { onMounted, ref } from 'vue';
-import { subDays, addDays, format  } from 'date-fns';
+import { subDays, addDays, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
 const daysOfTheWeek = ref([]);
